1ddcd945a44cbd3bf5a1158d2aa482f325bd506f
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-05 22:29:11 +0530

    Update 27-RemoveElement.java

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,13 +1,12 @@
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] != val) {
-                nums[count] = nums[i];
-                count++;
-            }
-        }
-        return count;
-    }
-        int count = 0;
+classÂ SolutionÂ {
+Â Â Â Â publicÂ intÂ removeElement(int[]Â nums,Â intÂ val)Â {
+Â Â Â Â Â Â Â Â int count = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+Â Â Â Â }
 }
-[
