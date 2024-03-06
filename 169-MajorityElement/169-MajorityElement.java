86bcf167dc681f2bcecc420e404b32a2e6cc256f
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-06 23:13:51 +0530

    Update 169-MajorityElement.java

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,14 +1,18 @@
-        //     if(frequency.get(nums[i]) > nums.length/2) {
-        //         return nums[i];
-        // }
-        // return -1;
+class Solution {
+    public int majorityElement(int[] nums) {
+        // SOLUTION 1
+        // Map<Integer, Integer> frequency = new HashMap<>();
+        // for(int i=0; i<nums.length;i++) {
+        //     frequency.put(nums[i], frequency.getOrDefault(nums[i],0)+1);
+        //     if(frequency.get(nums[i]) > nums.length/2) {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
 
-        //     }
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-        //     frequency.put(nums[i], frequency.getOrDefault(nums[i],0)+1);
-    }
 
-        // SOLUTION 2
+        // SOLUTION 2
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
 }
-[
