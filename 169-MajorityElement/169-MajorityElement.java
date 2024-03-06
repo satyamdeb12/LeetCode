10805a7752a1268b25da94ce42e89f85a44895fd
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-06 23:12:51 +0530

    Update file 06/03/2024, 23:12:51

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -5,7 +5,7 @@
 
         //     }
         Arrays.sort(nums);
-        return nums[nums.length/2]
+        return nums[nums.length/2];
         //     frequency.put(nums[i], frequency.getOrDefault(nums[i],0)+1);
     }
 
