06e82ef2de49965de22b2104ca641f14a421a248
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-06 10:19:06 +0530

    Create file

---
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,17 @@
+            } else {
+                 if(i == nums.length-2) { // second last element
+                    if(nums[i] == nums [i+1]) {
+                        nums[count] = nums[i];
+                }
+                    count++;
+                    nums[count] = nums[i];
+                if(nums[i] != nums[i+1]) {
+            }
+            if(i+1 < nums.length-1) {
+        for(int i = 0; i< nums.length; i++) {
+            if(nums.length < 2) {
+                return nums.length;
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+class Solution {
+[
