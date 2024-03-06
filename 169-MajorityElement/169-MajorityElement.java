ea17cfd8e487f2feb440072f16e7db5922817717
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-06 23:06:35 +0530

    Update file 06/03/2024, 23:06:35

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,15 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> frequency = new HashMap<>();
-        int max = nums[0];
-        for(int i=0; i<nums.length;i++) {
-            frequency.put(nums[i], frequency.getOrDefault(nums[i],0)+1);
-            if(frequency.get(nums[i]) > frequency.get(max)) {
-                max = nums[i];
-                if(frequency.get(max) > nums.length/2) return max;
-            }
-        }
-        return max;
+        // Map<Integer, Integer> frequency = new HashMap<>();
+        // int max = nums[0];
+        // for(int i=0; i<nums.length;i++) {
+        //     frequency.put(nums[i], frequency.getOrDefault(nums[i],0)+1);
+        //     if(frequency.get(nums[i]) > frequency.get(max)) {
+        //         max = nums[i];
+        //         if(frequency.get(max) > nums.length/2) return max;
+        //     }
+        // }
+        // return max;
     }
-}
+
+        Arrays.sort(nums);
+        return nums[nums.length/2];
 [
