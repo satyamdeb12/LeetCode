365b64118a4d83fd2ec1e22b825990bc9435a93d
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-07 23:36:43 +0530

    Update 121-BestTimetoBuyandSellStock.java

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,17 +1,18 @@
-        int leastSoFar = Integer.MAX_VALUE;
-        int profitIfSoldToday = 0;
-        int maximumProf = 0;
+class Solution {
+    public int maxProfit(int[] prices){
+        int leastSoFar = Integer.MAX_VALUE;
+        int profitIfSoldToday = 0;
+        int maximumProf = 0;
 
-        for(int i=0; i<prices.length; i++) {
-            if(prices[i] < leastSoFar) {
-                leastSoFar = prices[i];
-            }
-            profitIfSoldToday = prices[i] - leastSoFar;
-            if(maximumProf < profitIfSoldToday) {
-                maximumProf = profitIfSoldToday;
-            }
-        }
-        return maximumProf;
-    public int maxProfit(int[] prices){
-class Solution {
-[
+        for(int i=0; i<prices.length; i++) {
+            if(prices[i] < leastSoFar) {
+                leastSoFar = prices[i];
+            }
+            profitIfSoldToday = prices[i] - leastSoFar;
+            if(maximumProf < profitIfSoldToday) {
+                maximumProf = profitIfSoldToday;
+            }
+        }
+        return maximumProf;
+    }
+}
