7991089ba7d159e456c9a50fd8104b7cea7d9ed6
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-08 21:56:31 +0530

    Update 189-RotateArray.java

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,17 +1,23 @@
-        if(r<len && len>1){
-            int l=0;
-            while( l<len ){
-                nums[l] = copy[r];
-                l+=1;
-                r+=1;
-                if(r == len){
-                    if(r == 2){
-                        r = r%(len-1);
-                    } else {
-                        r = r%(len-1) -1;
-                    }
-                }
-            }
-        }
-    }
-[
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int[] copy = nums.clone();
+        int r = len-(k%len);
+
+        if(r<len && len>1){
+            int l=0;
+            while( l<len ){
+                nums[l] = copy[r];
+                l+=1;
+                r+=1;
+                if(r == len){
+                    if(r == 2){
+                        r = r%(len-1);
+                    } else {
+                        r = r%(len-1) -1;
+                    }
+                }
+            }
+        }
+    }
+}
