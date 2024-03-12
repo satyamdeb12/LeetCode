d1f7e8a28719fff87050a7753427097741b63e25
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-12 22:04:59 +0530

    Update 20-ValidParentheses.java

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,17 +1,16 @@
-            if (c == '(') 
-                stack.push(')'); 
-            else if (c == '{') 
-                stack.push('}'); 
-            else if (c == '[') 
-                stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c) 
-                return false;
-        }
-        return stack.isEmpty();
-    }
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>(); // create an empty stack
+        for (char c : s.toCharArray()) {
+            if (c == '(') 
+                stack.push(')'); 
+            else if (c == '{') 
+                stack.push('}'); 
+            else if (c == '[') 
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) 
+                return false;
+        }
+        return stack.isEmpty();
+    }
 }
-        for (char c : s.toCharArray()) {
-        Stack<Character> stack = new Stack<Character>(); // create an empty stack
-    public boolean isValid(String s) {
-class Solution {
-"()"
