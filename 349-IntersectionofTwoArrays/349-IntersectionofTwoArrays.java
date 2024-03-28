6b7e508e8a789ad4ff232e091fe6fee4112b6bed
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-29 00:58:16 +0530

    Create file

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
@@ -0,0 +1,13 @@
+                }
+            }
+        }
+        int[] intersectionArray = new int[result.size()];
+        int index = 0;
+        for (int num : result) {
+            intersectionArray[index++] = num;
+        }
+
+        return intersectionArray;
+    }
+}
+[
