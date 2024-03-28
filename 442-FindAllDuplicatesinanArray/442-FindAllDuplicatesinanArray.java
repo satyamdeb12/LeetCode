5f3208876a0b6f6974f939066de0412de85aaff3
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-03-28 23:09:07 +0530

    Update file 28/03/2024, 23:09:07

--- 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.java
+++ 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.java
@@ -2,14 +2,16 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         boolean[] isEncountered = new boolean[nums.length];
         List<Integer> result = new ArrayList<>();
-        for(int num : nums){
+
+        for(int i = 0; i< nums.length; i++){
+            int num = nums[i];
             if(isEncountered[num-1]){
                 result.add(num);
             } else{
                 isEncountered[num-1] = true;
+        return result;
             }
         }
-        return result;
     }
 }
 [
