be66d93290cd0b4dc8577417ede0af79168b3d90
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-04-03 00:20:07 +0530

    Create file

---
+++ 69-Sqrt(x)/69-Sqrt(x).java
@@ -0,0 +1,17 @@
+            if(square > x){
+                e = mid-1;
+            }
+            if(square < x) {
+            }
+                return mid;
+            if( square == x){
+            long square = (long) mid*mid;
+
+        while(s<=e){
+        int ans = -1;
+        int mid = s + (e-s)/2;
+        int e = x;
+         int s = 0;
+    public int mySqrt(int x) {
+class Solution {
+4
