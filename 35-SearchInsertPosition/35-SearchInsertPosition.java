d51cb3274cff96bce5b098696a884474bc4d92d7
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-04-11 23:14:51 +0530

    Create file

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,16 @@
+          return mid;
+        }
+        else if(target < nums[mid]){
+          end = mid-1;
+        } else {
+          start = mid+1;
+        }
+        mid = start + (end-start)/2;
+        if(target == nums[mid]){
+      while(start<=end){
+
+      }
+      return mid;
+    }
+}
+[
