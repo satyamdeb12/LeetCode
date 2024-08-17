eddd64b6e0b58774532889497933a7151ffaab77
SatyamDeb <43174719+satyamdeb12@users.noreply.github.com>
2024-08-17 23:10:34 +0530

    Create file

---
+++ 15-3Sum/15-3Sum.java
@@ -0,0 +1,25 @@
+            int k = n - 1;
+
+            int j = i + 1;
+            if((i > 0) && (nums[i]==nums[i-1])) continue;
+
+        for(int i = 0; i < n; i++) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
+
+class Solution {
+
+import java.util.*;
+[
+[-1,0,1,2,-1,-4]
+[0,1,1]
+[0,0,0]
+[[-1,-1,2],[-1,0,1]]
+[]
+[[0,0,0]]
+[[-1,-1,2],[-1,0,1]]
+[]
+[[0,0,0]]
